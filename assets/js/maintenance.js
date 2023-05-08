93cadee68f85add65e53147485b0817a225fa937
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-05-08 22:57:54 +0530

    final

---
+++ assets/js/maintenance.js
@@ -0,0 +1,15 @@
+/*  
+// Get the modal
+  var modal = document.getElementById("modal");
+
+  // Display the modal when the page is loaded
+  window.onload = function() {
+    modal.style.display = "block";
+  }
+
+  // Close the modal when the user clicks anywhere outside of it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
